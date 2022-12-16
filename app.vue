8d6c0a4bd11156d000dc2f6f69f751461c139626
Harlan Wilton <harlan@harlanzw.com>
2022-12-17 02:28:34 +1100

    test

--- app.vue
+++ app.vue
@@ -1,31 +1,31 @@
 <script lang="ts" setup>
 if (process.server) {
   console.log('app.vue')
-  const runtimeConfig = useRuntimeConfig()
-  console.log('runtime config', runtimeConfig)
-// get the current time nicely formatted for a human
-// this is the HTTP header that adds bearer token authentication
-  const usernames = runtimeConfig.username
-  const userFields = 'created_at,description,public_metrics,profile_image_url'
-  const data = await fetch(`https://api.twitter.com/2/users/by?usernames=${usernames}&user.fields=${userFields}`, {
-    headers: {
-      "User-Agent": "v2UserLookupJS",
-      "authorization": `Bearer ${runtimeConfig.token}`
-    },
-  })
-  console.log(await data.json())
-  defineOgImage({
-    runtime: true,
-    component: 'Banner',
-    time: new Date().toLocaleString('en-US', {
-      day: '2-digit',
-      month: 'short',
-      hour: 'numeric',
-      minute: 'numeric',
-      hour12: true
-    }),
-    followsCount: 0 //data?.data[0]?.public_metrics?.followers_count || 0,
-  })
+//   const runtimeConfig = useRuntimeConfig()
+//   console.log('runtime config', runtimeConfig)
+// // get the current time nicely formatted for a human
+// // this is the HTTP header that adds bearer token authentication
+//   const usernames = runtimeConfig.username
+//   const userFields = 'created_at,description,public_metrics,profile_image_url'
+//   const data = await fetch(`https://api.twitter.com/2/users/by?usernames=${usernames}&user.fields=${userFields}`, {
+//     headers: {
+//       "User-Agent": "v2UserLookupJS",
+//       "authorization": `Bearer ${runtimeConfig.token}`
+//     },
+//   })
+//   console.log(await data.json())
+  // defineOgImage({
+  //   runtime: true,
+  //   component: 'Banner',
+  //   time: new Date().toLocaleString('en-US', {
+  //     day: '2-digit',
+  //     month: 'short',
+  //     hour: 'numeric',
+  //     minute: 'numeric',
+  //     hour12: true
+  //   }),
+  //   followsCount: 0 //data?.data[0]?.public_metrics?.followers_count || 0,
+  // })
 }
 </script>
 <template>
--- server/routes/og-image.ts
+++ server/routes/og-image.ts
@@ -1,27 +1,26 @@
-// import edgeChromium from 'chrome-aws-lambda'
-// import puppeteer from 'puppeteer-core'
-import { defineEventHandler } from "h3";
+import edgeChromium from 'chrome-aws-lambda'
+import puppeteer from 'puppeteer-core'
+import { setHeader, defineEventHandler } from "h3";
 
-// async function createBrowser() {
-//     return puppeteer.launch({
-//         args: edgeChromium.args,
-//         executablePath: await edgeChromium.executablePath,
-//         headless: true,
-//     })
-// }
+async function createBrowser() {
+    return puppeteer.launch({
+        args: edgeChromium.args,
+        executablePath: await edgeChromium.executablePath,
+        headless: true,
+    })
+}
 
 export default defineEventHandler(async (e) => {
-    return 'hello'
-    // const browser = createBrowser()
-    // const page = await browser.newPage()
-    // await page.setViewportSize({
-    //     width: 1200,
-    //     height: 630,
-    // })
-    // await page.goto('https://nuxt-edge-og-image-demo.vercel.app/', {
-    //     timeout: 10000,
-    //     waitUntil: 'networkidle',
-    // })
-    // setHeader(e, 'Content-Type', 'image/png')
-    // return await page.screenshot()
+    const browser = createBrowser()
+    const page = await browser.newPage()
+    await page.setViewportSize({
+        width: 1200,
+        height: 630,
+    })
+    await page.goto('https://nuxt-edge-og-image-demo.vercel.app/', {
+        timeout: 10000,
+        waitUntil: 'networkidle',
+    })
+    setHeader(e, 'Content-Type', 'image/png')
+    return await page.screenshot()
 })
