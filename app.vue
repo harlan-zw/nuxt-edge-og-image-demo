d80d449dc769d4e465e51bf785c476ede3e4fb7e
Harlan Wilton <harlan@harlanzw.com>
2022-12-17 01:49:23 +1100

    fix: use fetch over ofetch

--- app.vue
+++ app.vue
@@ -5,11 +5,9 @@ if (process.server) {
   console.log('runtime config', runtimeConfig)
 // get the current time nicely formatted for a human
 // this is the HTTP header that adds bearer token authentication
-  const data = await $fetch('https://api.twitter.com/2/users/by', {
-    query: {
-      usernames: runtimeConfig.username,
-      'user.fields': 'created_at,description,public_metrics,profile_image_url',
-    },
+  const usernames = runtimeConfig.username
+  const userFields = 'created_at,description,public_metrics,profile_image_url'
+  const data = await fetch(`https://api.twitter.com/2/users/by?usernames=${usernames}&user.fields=${userFields}`, {
     headers: {
       "User-Agent": "v2UserLookupJS",
       "authorization": `Bearer ${runtimeConfig.token}`
