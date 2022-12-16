e7bca9e7d04048d63c019adf20d53b836d7463b1
Harlan Wilton <harlan@harlanzw.com>
2022-12-17 01:15:03 +1100

    test specific endpoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        'nuxt-og-image'
+    // 'nuxt-og-image'
     ],
 
     runtimeConfig: {
