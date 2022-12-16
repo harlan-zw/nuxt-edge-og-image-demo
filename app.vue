fba43d64e0cec74aa3c7e4f47847e80851e2f0fe
Harlan Wilton <harlan@harlanzw.com>
2022-12-17 01:37:10 +1100

    fix test #2

--- app.vue
+++ app.vue
@@ -1,3 +1,23 @@
+<script lang="ts" setup>
+if (process.server) {
+  console.log('app.vue')
+  const runtimeConfig = useRuntimeConfig()
+  console.log('runtime config', runtimeConfig)
+// get the current time nicely formatted for a human
+// this is the HTTP header that adds bearer token authentication
+  const data = await $fetch('https://api.twitter.com/2/users/by', {
+    query: {
+      usernames: runtimeConfig.username,
+      'user.fields': 'created_at,description,public_metrics,profile_image_url',
+    },
+    headers: {
+      "User-Agent": "v2UserLookupJS",
+      "authorization": `Bearer ${runtimeConfig.token}`
+    },
+  })
+  console.log(data)
+}
+</script>
 <template>
   <div>
     <NuxtWelcome />
